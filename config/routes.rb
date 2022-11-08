1796a4a831d8f1407bcb202decb6c96c684ed99f
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-11-08 14:26:45 +0000

    [finish]データベース作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+
+  root to: 'public/homes#top'
+  get "home/about" => "homes#about", as: "about"
+
+
+  devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  resources :customers
+
+  devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+  resources :admins
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
