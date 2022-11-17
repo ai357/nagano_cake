a865c68b0bb033e970ad1e01a313e45eebf8406b
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-11-17 06:41:54 +0000

    [intermediate]管理側＿注文履歴一覧

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,17 @@
 class Admin::HomesController < ApplicationController
 
-def top
-end
+  def top
+    @orders = Order.all
+    @order_details_page = Order_detail.all.page(params[:page])
+  end
+  
+  
+  private
+  
+  def order_details_params
+    params.require(:order_detail).permit(:name, :created_at, :amount, :making_status)
+  end
+
 
 end
+
