38c66a39377c74488dabce6e3b33cb27ac23de44
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2022-12-11 09:52:20 +0000

    [finish]サンクスページ注文履歴一覧

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -29,6 +29,39 @@ class Public::OrdersController < ApplicationController
   end
 
 
+  def create
+      #空のカラムを用意して情報を受け取る用の記述　@order = Order.new
+      @order = Order.new(order_params)
+      @order.customer_id = current_customer.id
+      @order.shipping_cost = 800
+      @order.status = 0
+      @order.save
+      current_customer.cart_items.each do |cart_item|
+        #一度パラメーターされたデータなのでパラメーター不要。
+        order_detail = OrderDetail.new
+        #order_detailテーブルの中を一つずつ埋める。（アプリ上で入力しないのでコントローラーで移す）
+        order_detail.order_id = @order.id
+        order_detail.item_id = cart_item.item_id
+        order_detail.price = (cart_item.item.price*1.1).to_i
+        order_detail.amount = cart_item.amount
+        #enumで0
+        order_detail.making_status = 0
+        order_detail.save
+      end
+      current_customer.cart_items.destroy_all
+      redirect_to orders_complete_path
+  end
+
+
+  def index
+      @orders = current_customer.orders
+  end
+
+
+  def show
+
+  end
+
 
 
     private
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,8 @@
 class Order < ApplicationRecord
-  
-  has_many :order_detail
-  
+
+  has_many :order_details
+
   enum payment_method: { credit_card:0, transfer:1 }
-  
+  enum status: { wait:0, confirm:1, making:2, prepar:3, done:4 }
+
 end
