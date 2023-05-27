a7f0803d4021e06127ce179b4445493c6b5344f9
Ai Hotokebuchi <112920373+ai357@users.noreply.github.com>
2023-05-27 12:35:31 +0000

    cart_item/public/controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -24,7 +24,7 @@ class Public::CartItemsController < ApplicationController
     redirect_to cart_items_path(@cart_item.id)
   end
 
-  # 1件のみ削除
+  # 買い物カゴ内の1件のみ削除
   def destroy
     @cart_items = CartItem.find(params[:id])
     @cart_items.destroy
